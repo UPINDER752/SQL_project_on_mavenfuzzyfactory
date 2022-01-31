2a5dba8ae18c0288e1fc80e008d29bc38d933368
UPINDER752 <98512970+UPINDER752@users.noreply.github.com>
2022-01-31 16:46:57 +0530

    Add files via upload

---
+++ Question.sql
@@ -0,0 +1,47 @@
+Assignment 1: You have received the below mail from your CEO and you have to respond to that
+mail.
+Hi,
+Our portal is live and it’s been quite some time now. Could you please help me in understanding
+from where the bulk of our website sessions are coming from?
+I want to see specifically breakdown of UTM source, campaign and referring domain.
+Cheers!
+From CEO,
+Date: 12th April 2012
+
+Assignment 2:
+It looks like gsearch nonbrand is the major traffic source, but we need to understand if we are get
+sales out of it.
+Is it possible for you to calculate the conversion rate from session to order?
+We will require to manage bids based on CVR
+Cheers!
+From EA - CEO,
+Date: 14th April 2012
+
+Assignment 3:
+Hey,
+Based on our last conversation where we analyzed conversion rate, we bid down gsearch non brand
+on 15th April 2012 because we were over bidding for g search non brand.
+Now, can you find gsearch non brand trended session, volume by week to see if the bid changes has
+caused the volume to drop at all?
+From Marketing Director,
+Date: 10th May 2012
+
+Assignment 4
+Hi There,
+I was just going through the mobile and realized that the UI is not that great, I did not have the
+satisfactory experience.
+Can you figure out the conversion rates from session to order by device type?
+In case the performance is better for desktop then we will bid more for desktop to bring more
+volume.
+From Marketing Director
+Date: 11th May 2012
+
+Assignment 5:
+Hi There,
+Based on device level analysis of conversion rates, desktop was doing well, so we raised the bid for
+gsearch nonbrand desktop on 19th
+May 2012.
+Can you figure out weekly trends by device type to see the impact on volume?
+Baseline: 15th April 2012.
+From Marketing Director
+Date: 9th June 2012
